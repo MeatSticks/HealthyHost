891616f659bd1941993ffd8da6e71a3dcf3622d0
dher4 <dher4@ucmerced.edu>
2017-10-08 17:28:50 -0700

    Update Spanish translation pages
    Spanish Local Clinics and Spanish Antiobitics

---
+++ app/src/main/java/healthyhostapp/healthyhost/spanlocal_clinics.java
@@ -0,0 +1,15 @@
+package healthyhostapp.healthyhost;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by herda on 10/6/2017.
+ */
+
+public class spanlocal_clinics extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spanlocal_clinics);
+    }
+}
