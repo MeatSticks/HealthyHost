2f2cbac6f65af7df90ba5cba83b702b4372ef208
Saigeetha Bhaskar <31899825+Sbhaskar114@users.noreply.github.com>
2017-10-10 10:54:32 -0700

    spanish backgroundinfo
    spanish background info is done but is not linking properly

---
+++ app/src/main/java/healthyhostapp/healthyhost/Spanish_Backgroundinfo.java
@@ -0,0 +1,15 @@
+package healthyhostapp.healthyhost;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Saigeetha on 10/10/17.
+ */
+
+public class Spanish_Backgroundinfo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spanish_backgroundinfo);
+    }
+}
