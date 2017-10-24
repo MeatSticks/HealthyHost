205eaccb952222859797665bde47b55b1f08c562
mandemeskel <mandemeskel@users.noreply.github.com>
2017-10-24 09:55:10 -0700

    added audio

--- app/src/main/java/healthyhostapp/healthyhost/amoxcillin.java
+++ app/src/main/java/healthyhostapp/healthyhost/amoxcillin.java
@@ -1,5 +1,6 @@
 package healthyhostapp.healthyhost;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,5 +13,8 @@ public class amoxcillin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.amoxcillin);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.moonlight);
+        mediaPlayer.start(); // no need to call prepare(); create() does that for you
     }
 }
