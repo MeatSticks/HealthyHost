321bfbab79b2d1ab92dbc2ebb299b9a5a9f7987b
dher4 <dher4@ucmerced.edu>
2017-11-14 10:45:48 -0800

    Hmong Antibiotic 6-10
    Erythromycin

---
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Antibiotic_Erythromycin.java
@@ -0,0 +1,17 @@
+package healthyhostapp.healthyhost;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import healthyhostapp.healthyhost.R;
+
+/**
+ * Created by herda on 11/14/2017.
+ */
+
+public class Hmong_Antibiotic_Erythromycin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hmong_antibiotic_erythromycin);
+    }
+}
