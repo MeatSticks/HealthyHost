acc5a4f946ee8ad6641bcc8626605a01eb5d42ed
dher4 <dher4@ucmerced.edu>
2017-11-16 20:16:52 -0800

    Hmong Antibiotics pages
    Hmong Antibiotics Pages

---
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Antibiotic_Doxycycline.java
@@ -0,0 +1,15 @@
+package healthyhostapp.healthyhost;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by herda on 11/16/2017.
+ */
+public class Hmong_Antibiotic_Doxycycline extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hmong_antibiotic_doxycycline);
+    }
+}
