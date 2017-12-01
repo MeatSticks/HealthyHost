f6606df53fa7a1e3de44dde207ed4cb72e400f08
mandemeskel <mandemeskel@users.noreply.github.com>
2017-11-30 20:19:31 -0800

    added fragment to mediplayer class and added on click listners

--- app/src/main/java/healthyhostapp/healthyhost/AudioPlayer.java
+++ app/src/main/java/healthyhostapp/healthyhost/AudioPlayer.java
@@ -1,73 +1,133 @@
 package healthyhostapp.healthyhost;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
- * Created by guest on 11/28/17.
+ * Created by Michael on 11/28/17.
  */
 
-public class AudioPlayer {
+public class AudioPlayer extends Fragment {
     private static AudioPlayer player;
     private Context context;
     private MediaPlayer mediaPlayer;
     private int audioFile;
 
-    // TODO: should be a singleton
-    private AudioPlayer(Context context, int audioFile) {
-        this.context = context;
-        this.audioFile = audioFile;
-
-        // load the audio in /raw folder called moonlight
-        mediaPlayer = MediaPlayer.create(this.context, audioFile);
-        // loop the audio to play when it ends
-        mediaPlayer.setLooping(true);
+    public AudioPlayer() {
+        // Required empty public constructor
     }
 
     public AudioPlayer CreateAudioPlayer(Context context, int audioFile) {
         if(player == null) {
-            player = new AudioPlayer(context, audioFile);
+            player = new AudioPlayer();
+            player.context = context;
+            player.audioFile = audioFile;
         }
         return player;
     }
 
-    // inserts player interface fragment into the activity's UI
-    private void insertPlayerInterface() {
-
+    private void setupMediaPlayer() {
+        // load the audio in /raw folder called moonlight
+        mediaPlayer = MediaPlayer.create(context, audioFile);
+        // loop the audio to play when it ends
+        mediaPlayer.setLooping(true);
     }
 
-    // insert player interface into specified activity's UI
-    public void insertPlayerInterface(Context context) {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupMediaPlayer();
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_audio_player, container, false);
+
+        Button play = (Button) view.findViewById(R.id.btnPlayAudio);
+        play.setOnClickListener(btnClick);
+        Button pause = (Button) view.findViewById(R.id.btnPauseAudio);
+        pause.setOnClickListener(btnClick);
+        Button reset = (Button) view.findViewById(R.id.btnResetAudio);
+        reset.setOnClickListener(btnClick);
+
+        return view;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mediaPlayer.release();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.setupMediaPlayer();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 
     // play audio, if it is already playing reset it
     public void playAudio() {
-        if(this.mediaPlayer.isPlaying() == false)
-            this.mediaPlayer.start(); // no need to call prepare(); create() does that for you
+        if(mediaPlayer.isPlaying() == false)
+            mediaPlayer.start(); // no need to call prepare(); create() does that for you
         else
             resetAudio();
     }
 
     // reset audio to 0sec and lay it
     public void resetAudio() {
-        this.mediaPlayer.seekTo(0);
-        this.mediaPlayer.start();
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
     }
 
     // pause the video if it is playing
     public void pauseAudio() {
-        if(this.mediaPlayer.isPlaying())
-            this.mediaPlayer.pause();
+        if(mediaPlayer.isPlaying())
+            mediaPlayer.pause();
     }
+
+    // the button click handlers for play, pause, and reset buttons
+    final View.OnClickListener btnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.btnPlayAudio:
+                    playAudio();
+                    break;
+                case R.id.btnPauseAudio:
+                    pauseAudio();
+                    break;
+                case R.id.btnResetAudio:
+                    resetAudio();
+                    break;
+            }
+        }
+    };
 
     // release media player
     public void releasePlayer() {
-        this.mediaPlayer.release();
+        mediaPlayer.release();
     }
 
     // set the audio track that will be played
     public void setTrack() {
-
     }
 }
