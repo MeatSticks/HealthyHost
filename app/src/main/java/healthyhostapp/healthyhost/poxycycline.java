8b797720a15b7c30040f0ff0dcd38fee92e23afa
TheJetro <TheJetro@users.noreply.github.com>
2018-02-13 22:10:19 -0800

    Style changes to Antibiotics
    I changed the style of all of the antibiotics into the same style.  The style of the antibiotics can all now be changed within styles.xml.  Also the penecillinG and the doxycyline file names were changed from gpill and poxycyline persepectively.

--- app/src/main/java/healthyhostapp/healthyhost/poxycycline.java
+++ app/src/main/java/healthyhostapp/healthyhost/poxycycline.java
@@ -11,6 +11,6 @@ public class poxycycline extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.poxycline);
+        setContentView(R.layout.doxycline);
     }
 }
