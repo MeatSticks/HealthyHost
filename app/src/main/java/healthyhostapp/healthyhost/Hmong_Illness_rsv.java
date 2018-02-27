1e03d06c1b0f087915e8779cf858fc52a02e9179
Neel Patel <patelneel1499@gmail.com>
2018-02-27 10:46:01 -0800

    corrected spelling rsv
    correct spelling on the hmong rsv page

--- app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_rsv.java
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_rsv.java
@@ -3,11 +3,11 @@ package healthyhostapp.healthyhost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Hmong_Ilness_rsv extends AppCompatActivity {
+public class Hmong_Illness_rsv extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hmong_ilness_rsv);
+        setContentView(R.layout.hmong_illness_rsv);
     }
 }
