1338b17c919eb37a49911c52473d9f25c7db3998
Neel Patel <patelneel1499@gmail.com>
2018-02-27 10:35:28 -0800

    added rsv
    added rsv hmong illness page

---
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Ilness_rsv.java
@@ -0,0 +1,13 @@
+package healthyhostapp.healthyhost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hmong_Ilness_rsv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hmong_ilness_rsv);
+    }
+}
