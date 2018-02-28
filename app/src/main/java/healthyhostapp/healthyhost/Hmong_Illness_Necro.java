42413bde86894a3e9219c80fd83e34ab3325aed8
Neel Patel <patelneel1499@gmail.com>
2018-02-27 21:45:44 -0800

    corrected the spelling for hmong
    corrected the spelling for hmong on the branch and using the refactor option

--- app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_Necro.java
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_Necro.java
@@ -3,11 +3,11 @@ package healthyhostapp.healthyhost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Hmog_Illness_Necro extends AppCompatActivity {
+public class Hmong_Illness_Necro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hmog_illness_necro);
+        setContentView(R.layout.hmong_illness_necro);
     }
 }
