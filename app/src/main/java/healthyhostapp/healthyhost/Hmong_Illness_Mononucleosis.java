d4abbb66e6eadb90740dd182f71b82f518a13917
Arnold Vasquez <ArnoldVasquez@ucmerced-10-34-0-56.ucmerced.edu>
2018-03-03 21:10:49 -0800

    Added audio files to pages
    added audio files to illenesses
    impetigo
    mono
    rsv
    strep throat
    uti

--- app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_Mononucleosis.java
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_Mononucleosis.java
@@ -9,5 +9,6 @@ public class Hmong_Illness_Mononucleosis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hmong_illness_mononucleosis);
+        AudioPlayer.CreateAudioPlayer(this, R.raw.hmong_mono)
     }
 }
--- app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_uti.java
+++ app/src/main/java/healthyhostapp/healthyhost/Hmong_Illness_uti.java
@@ -9,5 +9,6 @@ public class Hmong_Illness_uti extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hmong_illness_uti);
+        AudioPlayer.CreateAudioPlayer(this,R.raw.hmong_uti)
     }
 }
