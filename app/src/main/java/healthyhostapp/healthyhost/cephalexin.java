d4efeb1561f4498740ff5f875c2da1c304b9a5a5
Neel Patel <patelneel1499@gmail.com>
2018-03-04 11:50:25 -0800

    added audio cephalexin
    linked audio english_antibio7

--- app/src/main/java/healthyhostapp/healthyhost/cephalexin.java
+++ app/src/main/java/healthyhostapp/healthyhost/cephalexin.java
@@ -12,5 +12,7 @@ public class cephalexin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cephalexin);
+        AudioPlayer.CreateAudioPlayer(this, R.raw.english_antibio7);
+
     }
 }
