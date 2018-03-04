cb2205b995523546d926f7db8d1253e84bd28bf2
Neel Patel <patelneel1499@gmail.com>
2018-03-04 11:45:02 -0800

    Revert "added audio cephalexin"
    This reverts commit 4d4eb81ebd78c1f4993d88c8a14776e2d2c3c913.

--- app/src/main/java/healthyhostapp/healthyhost/cephalexin.java
+++ app/src/main/java/healthyhostapp/healthyhost/cephalexin.java
@@ -12,7 +12,5 @@ public class cephalexin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cephalexin);
-        AudioPlayer.CreateAudioPlayer(this, R.raw.hmong_ceftriaxone);
-
     }
 }
