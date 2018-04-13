a51176c07dca5dc038c74f4d24b556ee2530bb7e
Neel Patel <patelneel1499@gmail.com>
2018-04-12 20:42:13 -0700

    added list to java file
    added list of resources to java file

--- app/src/main/java/healthyhostapp/healthyhost/resources.java
+++ app/src/main/java/healthyhostapp/healthyhost/resources.java
@@ -8,6 +8,18 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class resources extends AppCompatActivity {
+    public final String[] resourcesURLS = {"www.cdc.gov",
+            "http://www.bpac.org.nz/Supplement/2013/July/antibiotics-guide.aspx#ear-nose-throat-sec",
+            "http://www.emedicinehealth.com/antibiotics/page2_em.htm",
+            "http://health.mo.gov/safety/antibioticresistance/",
+            "https://www.webmd.com/a-to-z-guides/bacterial-and-viral-infections",
+            "http://www.livestrong.com/article/84796-bacteria-infect/",
+            "http://www.emedicinehealth.com/drug-ofloxacin/article_em.htm#whatis",
+            "https://www.nlm.nih.gov/medlineplus/druginfo/meds/a685032.html",
+            "http://www.drugs.com/alpha/a1.html",
+            "http://www.cdc.gov/non-polio-enterovirus/about/EV-D68.html#",
+            "http://www.cdc.gov/non-polio-enterovirus/about/ev68-infographic.html#text"} ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
