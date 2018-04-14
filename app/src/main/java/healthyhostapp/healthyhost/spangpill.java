b6eef5b8a13984734549cdbc2d4f0cb45380bf7f
Arnold Vasquez <ArnoldVasquez@ucmerced-10-34-0-56.ucmerced.edu>
2018-04-13 22:58:40 -0700

    Added spanish antiobiotics files

--- app/src/main/java/healthyhostapp/healthyhost/spangpill.java
+++ app/src/main/java/healthyhostapp/healthyhost/spangpill.java
@@ -17,55 +17,6 @@ public class spangpill extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spangpill);
-
-        Button play = (Button) findViewById(R.id.btnPlayAudio);
-        play.setOnClickListener(btnClick);
-        Button pause = (Button) findViewById(R.id.btnPauseAudio);
-        pause.setOnClickListener(btnClick);
-        Button reset = (Button) findViewById(R.id.btnResetAudio);
-        reset.setOnClickListener(btnClick);
-
-        // load the audio in /raw folder called moonlight
-        mediaPlayer = MediaPlayer.create(this, R.raw.moonlight);
-        // loop the audio to play when it ends
-        mediaPlayer.setLooping(true);
+        AudioPlayer.CreateAudioPlayer(this, R.raw.spanish_anti_penicillin);
     }
-
-    // play audio, if it is already playing reset it
-    static void playAudio() {
-        if(mediaPlayer.isPlaying() == false)
-            mediaPlayer.start(); // no need to call prepare(); create() does that for you
-        else
-            resetAudio();
-    }
-
-    // reset audio to 0sec and lay it
-    static void resetAudio() {
-        mediaPlayer.seekTo(0);
-        mediaPlayer.start();
-    }
-
-    // pause the video if it is playing
-    static void pauseAudio() {
-        if(mediaPlayer.isPlaying())
-            mediaPlayer.pause();
-    }
-
-    // the button click handlers for play, pause, and reset buttons
-    final View.OnClickListener btnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch(view.getId()) {
-                case R.id.btnPlayAudio:
-                    playAudio();
-                    break;
-                case R.id.btnPauseAudio:
-                    pauseAudio();
-                    break;
-                case R.id.btnResetAudio:
-                    resetAudio();
-                    break;
-            }
-        }
-    };
 }
